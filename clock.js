a4b3e1cd0332e086566b765954357b6a03098b90
muhammadsodiq009 <miraxmatovmuhammadsodiq@gmail.com>
2023-08-17 16:32:20 +0500

    index.html

--- clock.js
+++ clock.js
@@ -14,9 +14,9 @@ console.log( sana.getMinutes);
 
 function formatTime() {
     let date = new Date()
-    let hour = date.getHours()
-    let minute = date.getMinutes()
-    let second = date.getSeconds()
+    let hour = date.getHours().toString().padStart(2, "0")
+    let minute = date.getMinutes().toString().padStart(2, "0")
+    let second = date.getSeconds().toString().padStart(2, "0")
     time.innerHTML= `${hour}:${minute}:${second}`
 }
 formatTime()
@@ -24,3 +24,33 @@ formatTime()
 setInterval(() => {
     formatTime()
 }, 1000);
+
+function formatDay (){
+    let date = new Date()
+    let index = date.getDay()
+    let dayName = ""
+    switch(index){
+        case 0:
+            dayName = "Sunday"
+            break
+        case 1:
+            dayName = "Monday"
+            break
+        case 2:
+            dayName = "Tuesday"
+            break
+        case 3:
+            dayName = "Wednesday"
+            break
+        case 4:
+            dayName = "Thursday"
+            break
+        case 5:
+            dayName = "Friday"
+            break
+        case 6:
+            dayName = "Saturday"
+            break
+    }
+    day.innerHTML = dayName
+}
